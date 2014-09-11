7df8d106dba8b32fe71e016e38dcb7662de6856b
Rafal Przywarski <rafalprzywarski@Rafals-MacBook-Pro.local>
2014-09-11 16:36:09 +0200

    fixed distribution bug

--- source/main.cpp
+++ source/main.cpp
@@ -10,7 +10,7 @@ lc::FixedDistribution circle(lc::Float radius, unsigned N)
     for (unsigned y = 0; y < N; ++y)
         for (unsigned x = 0; x < N; ++x)
         {
-            lc::Point p{(lc::Float(x) / N - 0.5) * radius, (lc::Float(y) / N - 0.5) * radius, 0};
+            lc::Point p{(lc::Float(x) / N - 0.5) * radius * 2, (lc::Float(y) / N - 0.5) * radius * 2, 0};
             if (p.length() > radius)
                 continue;
             p[2] = 1;
@@ -29,7 +29,7 @@ int main()
         {{ 30, 0, 260}, 15},
     };
     lc::LightSources lights = {{{0, 0, 0}, 5}};
-    lc::FixedDistribution distribution = circle(10, 32);
+    lc::FixedDistribution distribution = circle(5, 12);
     auto sensor = lc::createCameraSensor({1024, 512}, 0.0025, distribution);
     lc::ThinLens lens{2, 1.9775};
     auto camera = lc::createCamera(sensor, lens);
